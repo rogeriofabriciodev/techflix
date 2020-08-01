06cc9ad27be4f6c1d75e107ac68dbf73f50147a4
rogeriofabriciodev <60872513+rogeriofabriciodev@users.noreply.github.com>
2020-07-31 22:53:28 -0300

    Aula 03
    Server

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,32 +1,125 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import FormFields from '../../../components/FormFields';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+  const [categorias, setCategorias] = useState([valoresIniciais]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+
+  function handleChange(e) {
+    setValue(
+      e.target.getAttribute('name'),
+      e.target.value,
+    );
+  }
+
+  useEffect(() => {
+    const URL = 'http://localhost:5000/categorias';
+    fetch(URL)
+      .then(async (respostaDoServidor) => {
+        const resposta = await respostaDoServidor.json();
+        setCategorias([
+          ...resposta,
+        ]);
+      });
+    // setTimeout(() => {
+    //   setCategorias([
+    //     ...categorias,
+    //     {
+    //       id: 1,
+    //       nome: 'Front-end',
+    //       descricao: 'Uma categoria bacana',
+    //       cor: '#cbd1ff',
+    //     },
+    //     {
+    //       id: 2,
+    //       nome: 'Back-end',
+    //       descricao: 'Outra categoria bacana',
+    //       cor: '#cbd1ff',
+    //     },
+    //   ]);
+    // }, 4 * 1000);
+  },
+  []);
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+      <h1>
+        Cadastro de Categoria:
+        { values.nome }
+      </h1>
 
-      <form>
+      <form onSubmit={function handleSubmit(e) {
+        e.preventDefault();
 
-        <label>
-          Nome da Categoria:
-          <input
-            type="text"
-          />
-        </label>
+        setCategorias([...categorias, values]);
 
-        <button>
+        setValues(valoresIniciais);
+      }}
+      >
+
+        <FormFields
+          label="Nome da Categoria"
+          type="text"
+          value={values.nome}
+          name="nome"
+          onChange={handleChange}
+        />
+
+        <FormFields
+          label="Descrição"
+          type="textarea"
+          value={values.descricao}
+          name="descricao"
+          onChange={handleChange}
+        />
+
+        <FormFields
+          label="Cor"
+          type="color"
+          value={values.cor}
+          name="cor"
+          onChange={handleChange}
+        />
+
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
 
+      {categorias.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
+
+      <ul>
+        { categorias.map((categoria) => (
+          <li key={`${categoria.id}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
 
       <Link to="/">
         Ir para home
       </Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;
